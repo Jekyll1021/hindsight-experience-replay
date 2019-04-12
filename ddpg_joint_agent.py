54f7b8ddbc0bc8d623ce2fdd82fcf1da0338064a
Zisu Dong <jdong1021@fb.com>
2019-04-11 23:40:06 -0400

    recurrent

--- ddpg_joint_agent.py
+++ ddpg_joint_agent.py
@@ -220,7 +220,7 @@ class ddpg_joint_agent:
                        'ag_next': mb_ag_next,
                        'sg': mb_sg,
                        'sg_next': mb_sg_next,
-                       'hidden' = mb_hidden,
+                       'hidden': mb_hidden,
                        'hidden_next' = mb_hidden_next
                        }
         transitions = her_module.sample_her_transitions(buffer_temp, num_transitions)
