c95d4d6e02d37aeb6d3f9d84c78ebc924602b453
Zisu Dong <jdong1021@fb.com>
2019-04-23 17:45:05 -0400

    open loop

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from arguments import get_args
 from mpi4py import MPI
 from subprocess import CalledProcessError
 from ddpg_agent import ddpg_agent
+from open_loop_agent import open_loop_agent
 
 """
 train the agent, the MPI part code is copy from openai baselines(https://github.com/openai/baselines/blob/master/baselines/her)
@@ -23,12 +24,14 @@ def get_env_params(env):
 
 def launch(args):
     # create the ddpg_agent
-    env = gym.make(args.env_name, reward_type='sparse', goal_type='random', cam_type='fixed', gripper_init_type='random', act_noise=False, obs_noise=False)
+    env = gym.make(args.env_name, reward_type='sparse', goal_type='random', cam_type='random', gripper_init_type='random', act_noise=False, obs_noise=False)
     # get the environment parameters
     env_params = get_env_params(env)
     # create the ddpg agent to interact with the environment
-    ddpg_trainer = ddpg_agent(args, env, env_params)
-    ddpg_trainer.learn()
+    # ddpg_trainer = ddpg_agent(args, env, env_params)
+    # ddpg_trainer.learn()
+    open_loop_trainer = open_loop_agent(args, env, env_params)
+    open_loop_trainer.learn()
 
 if __name__ == '__main__':
     # take the configuration for the HER
