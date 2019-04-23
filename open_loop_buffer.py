a3effb3d88f11063d71deb5ab36067fd73206ec2
Zisu Dong <jdong1021@fb.com>
2019-04-23 18:03:33 -0400

    open loop

--- open_loop_buffer.py
+++ open_loop_buffer.py
@@ -7,14 +7,13 @@ the replay buffer here is basically from the openai baselines code
 
 """
 class open_loop_buffer:
-    def __init__(self, env_params, buffer_size, sample_size, cuda=True):
+    def __init__(self, env_params, buffer_size, sample_size):
         self.env_params = env_params
         self.size = buffer_size
         self.sample_size = sample_size
         # memory management
         self.current_size = 0
         self.n_transitions_stored = 0
-        self.cuda = cuda
         # create the buffer to store info
 
         self.buffers = {'obs': torch.zeros([self.size, self.sample_size, self.env_params['obs']]),
@@ -25,9 +24,8 @@ class open_loop_buffer:
         # thread lock
         self.lock = threading.Lock()
 
-        if cuda:
-            for key in self.buffers.keys():
-                self.buffers[key] = self.buffers[key].cuda()
+        for key in self.buffers.keys():
+            self.buffers[key] = self.buffers[key].cuda()
 
 
     # store the episode
@@ -36,8 +34,7 @@ class open_loop_buffer:
         sample_size = mb_obs.shape[0]
         with self.lock:
             idxs = self._get_storage_idx(inc=sample_size)
-            if self.cuda:
-                idxs = torch.LongTensor(idxs).cuda()
+            idxs = torch.LongTensor(idxs).cuda()
             # store the informations
             self.buffers['obs'][idxs] = mb_obs
             self.buffers['actions'][idxs] = mb_actions
