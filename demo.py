dcb5227a961152c9e4f67dfe6d520d33a893a464
Zisu Dong <jdong1021@fb.com>
2019-04-30 12:08:37 -0400

    accelaration

--- demo.py
+++ demo.py
@@ -5,28 +5,32 @@ import gym
 import numpy as np
 
 # process the inputs
-def process_inputs(o, g, o_mean, o_std, g_mean, g_std, args):
+# def process_inputs(o, g, o_mean, o_std, g_mean, g_std, args):
+def process_inputs(o, o_mean, o_std, args):
     o_clip = np.clip(o, -args.clip_obs, args.clip_obs)
-    g_clip = np.clip(g, -args.clip_obs, args.clip_obs)
+    # g_clip = np.clip(g, -args.clip_obs, args.clip_obs)
     o_norm = np.clip((o_clip - o_mean) / (o_std), -args.clip_range, args.clip_range)
-    g_norm = np.clip((g_clip - g_mean) / (g_std), -args.clip_range, args.clip_range)
-    inputs = np.concatenate([o_norm, g_norm])
+    # g_norm = np.clip((g_clip - g_mean) / (g_std), -args.clip_range, args.clip_range)
+    # inputs = np.concatenate([o_norm, g_norm])
+    inputs = np.array([o_norm])
     inputs = torch.tensor(inputs, dtype=torch.float32)
     return inputs
 
 if __name__ == '__main__':
     args = get_args()
+    use_image = True
     # load the model param
     model_path = args.save_dir + args.env_name + '/model.pt'
-    o_mean, o_std, g_mean, g_std, model = torch.load(model_path, map_location=lambda storage, loc: storage)
+    # o_mean, o_std, g_mean, g_std, model = torch.load(model_path, map_location=lambda storage, loc: storage)
+    o_mean, o_std, model = torch.load(model_path, map_location=lambda storage, loc: storage)
     # create the environment
     env = gym.make(args.env_name)
     # get the env param
     observation = env.reset()
     # get the environment params
-    env_params = {'obs': observation['observation'].shape[0], 
-                  'goal': observation['desired_goal'].shape[0], 
-                  'action': env.action_space.shape[0], 
+    env_params = {'obs': observation['observation'].shape[0],
+                  'goal': observation['desired_goal'].shape[0],
+                  'action': env.action_space.shape[0],
                   'action_max': env.action_space.high[0],
                   }
     # create the actor network
@@ -38,11 +42,17 @@ if __name__ == '__main__':
         # start to do the demo
         obs = observation['observation']
         g = observation['desired_goal']
+        img = observation['image']
         for t in range(env._max_episode_steps):
             env.render()
-            inputs = process_inputs(obs, g, o_mean, o_std, g_mean, g_std, args)
+            # inputs = process_inputs(obs, g, o_mean, o_std, g_mean, g_std, args)
+            inputs = process_inputs(obs, o_mean, o_std, args)
             with torch.no_grad():
-                pi = actor_network(inputs)
+                if use_image:
+                    image_tensor = torch.tensor([img], dtype=torch.float32)
+                    pi = actor_network(inputs, image_tensor)
+                else:
+                    pi = actor_network(inputs)
             action = pi.detach().numpy().squeeze()
             # put actions into the environment
             observation_new, reward, _, info = env.step(action)
