7a64dbe37b9914637b497daf992e29fe36a889c0
Zisu Dong <jdong1021@fb.com>
2019-05-01 17:40:10 -0400

    debug

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -242,6 +242,7 @@ class ddpg_agent:
         inputs_norm = obs_norm
         obs_next_norm = self.o_norm.normalize(transitions['obs_next'])
         inputs_next_norm = obs_next_norm
+        print("avg rewards {}".format(np.mean(transitions['r'])))
         # transfer them into the tensor
         inputs_norm_tensor = torch.tensor(inputs_norm, dtype=torch.float32)
         inputs_next_norm_tensor = torch.tensor(inputs_next_norm, dtype=torch.float32)
