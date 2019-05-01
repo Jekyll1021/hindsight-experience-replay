4e46381cb9d9efec4b0dda5012d8853c6ebadb33
Zisu Dong <jdong1021@fb.com>
2019-04-30 21:49:26 -0400

    resnet

--- models.py
+++ models.py
@@ -53,9 +53,9 @@ class critic(nn.Module):
 class actor_image(nn.Module):
     def __init__(self, env_params, input_num, output_num=4):
         super(actor_image, self).__init__()
-        self.feature_extraction_model = models.resnet18(pretrained=True).features.eval()
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
-        self.image_fc1 = nn.Linear(256 * 7 * 7, 4096)
+        self.feature_extraction_model = models.alexnet(pretrained=True).features.eval()
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.image_fc1 = nn.Linear(256 * 6 * 6, 4096)
         self.image_fc2 = nn.Linear(4096, 4096)
         self.image_fc3 = nn.Linear(4096, 64)
 
@@ -92,9 +92,11 @@ class critic_image(nn.Module):
         super(critic_image, self).__init__()
         self.max_action = env_params['action_max']
 
-        self.feature_extraction_model = models.alexnet(pretrained=True).features.eval()
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.image_fc1 = nn.Linear(256 * 6 * 6, 4096)
+        # self.feature_extraction_model = models.alexnet(pretrained=True).features.eval()
+        resnet = models.resnet50(pretrained=True).eval()
+        self.feature_extraction_model = nn.Sequential(list(resnet.children())[:-2])
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.image_fc1 = nn.Linear(256 * 7 * 7, 4096)
         self.image_fc2 = nn.Linear(4096, 4096)
         self.image_fc3 = nn.Linear(4096, 64)
 
