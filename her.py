1bf1b9407eaf60abd5d298ff2121dcd132142a84
Zisu Dong <jdong1021@fb.com>
2019-05-13 14:28:32 -0400

    batch time debug

--- her.py
+++ her.py
@@ -18,14 +18,14 @@ class her_sampler:
         episode_idxs = np.random.randint(0, rollout_batch_size, batch_size)
         t_samples = np.random.randint(T, size=batch_size)
         transitions = {key: episode_batch[key][episode_idxs, t_samples].copy() for key in episode_batch.keys()}
-        # her idx
-        her_indexes = np.where(np.random.uniform(size=batch_size) < self.future_p)
-        future_offset = np.random.uniform(size=batch_size) * (T - t_samples)
-        future_offset = future_offset.astype(int)
-        future_t = (t_samples + 1 + future_offset)[her_indexes]
-        # replace go with achieved goal
-        future_ag = episode_batch['ag'][episode_idxs[her_indexes], future_t]
-        transitions['g'][her_indexes] = future_ag
+        # # her idx
+        # her_indexes = np.where(np.random.uniform(size=batch_size) < self.future_p)
+        # future_offset = np.random.uniform(size=batch_size) * (T - t_samples)
+        # future_offset = future_offset.astype(int)
+        # future_t = (t_samples + 1 + future_offset)[her_indexes]
+        # # replace go with achieved goal
+        # future_ag = episode_batch['ag'][episode_idxs[her_indexes], future_t]
+        # transitions['g'][her_indexes] = future_ag
         # to get the params to re-compute reward
         transitions['r'] = np.expand_dims(self.reward_func(transitions['ag_next'], transitions['g'], info), 1)
 
